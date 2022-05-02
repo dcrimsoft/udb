0ecd7ab7982c012c72c02859879e908fd4594662
dcrimsoft <dcrimsoft@gmail.com>
2022-05-02 18:11:41 -0500

    8. deployment tings

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import ReactDOM from 'react-dom';
 import App from './components/App.js';
 import * as serviceWorker from './serviceWorker';
-import Iv_admin from './components/Iv_admin';
+import Iv_admin from './components/Iv_admin.js';
 
 import { BrowserRouter as Router, Routes, Switch, Route } from "react-router-dom";
 
@@ -11,8 +11,8 @@ import { BrowserRouter as Router, Routes, Switch, Route } from "react-router-dom
 ReactDOM.render(
     <Router>
         <Routes>
-            <Route path="/" element={ <App /> } />
-            <Route path="/abt/IV_Admin" element={ <Iv_admin /> } />
+            <Route exact path="/" element={ <App /> } />
+            <Route exact path="/abt/IV_Admin" element={ <Iv_admin /> } />
         </Routes>
     </Router>, 
 document.getElementById('root'));
