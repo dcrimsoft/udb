7647f1dc458c0ef97c15483cdb8eac29129de2b6
dcrimsoft <dcrimsoft@gmail.com>
2022-05-02 15:37:30 -0500

    2. still on deployment

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/udb/precache-manifest.7e8f93646b266df8450b1175c0639ccc.js"
+  "/udb/precache-manifest.3b22b6924d6bcd9795e3d02dd78ab836.js"
 );
 
 workbox.clientsClaim();
