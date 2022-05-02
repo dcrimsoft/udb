af546b8985e979411eb0551aa8a8f9420182213e
dcrimsoft <dcrimsoft@gmail.com>
2022-05-02 17:33:43 -0500

    Updates

---
+++ precache-manifest.6d1f1cbe0f9295014c0caadc439232e3.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb9f73d6652be1f6430f",
+    "url": "/udb/static/css/main.f7e4adaa.chunk.css"
+  },
+  {
+    "revision": "fb9f73d6652be1f6430f",
+    "url": "/udb/static/js/main.fb9f73d6.chunk.js"
+  },
+  {
+    "revision": "76ff6171c6f8c64ef050",
+    "url": "/udb/static/css/1.e3f95f43.chunk.css"
+  },
+  {
+    "revision": "76ff6171c6f8c64ef050",
+    "url": "/udb/static/js/1.76ff6171.chunk.js"
+  },
+  {
+    "revision": "0210241b86bed41d2f76",
+    "url": "/udb/static/js/runtime~main.0210241b.js"
+  },
+  {
+    "revision": "bc5dc96b3ec54e01dab2d037f8d4a674",
+    "url": "/udb/static/media/add_icon.bc5dc96b.PNG"
+  },
+  {
+    "revision": "308c420b6656412e73b121fbc0da67c9",
+    "url": "/udb/static/media/home_logo.308c420b.png"
+  },
+  {
+    "revision": "086fc202889da85a17c39b1eea653636",
+    "url": "/udb/static/media/udb.086fc202.png"
+  },
+  {
+    "revision": "ec10d60b8143f04205fc5946bd233320",
+    "url": "/udb/static/media/amateur_bg.ec10d60b.PNG"
+  },
+  {
+    "revision": "fd5ab76ffffad19abd24c717890a50b9",
+    "url": "/udb/static/media/survivor_bg.fd5ab76f.png"
+  },
+  {
+    "revision": "3b746597dfc0e2ff775a7d0e046ef9c6",
+    "url": "/udb/static/media/assassin_bg.3b746597.png"
+  },
+  {
+    "revision": "aef0822e000c7cb09be8728612b68af6",
+    "url": "/udb/static/media/z_killer.aef0822e.PNG"
+  },
+  {
+    "revision": "0e7bc5750f71095e9d42c958aa2d0c4b",
+    "url": "/udb/static/media/amateur.0e7bc575.PNG"
+  },
+  {
+    "revision": "118fd46f5dd181e185ceba4b177ecd7d",
+    "url": "/udb/static/media/main_bg.118fd46f.jpg"
+  },
+  {
+    "revision": "71ae60d8b81c407af699f8f5ab5ca41d",
+    "url": "/udb/index.html"
+  }
+];
